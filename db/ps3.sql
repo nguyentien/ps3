02354151312072417fc7e5db0553c70926ba2d4f
tiennguyen <tiennguyen@success-ss.com.vn>
2012-07-24 12:09:08 +0700

    - add db

---
+++ db/ps3.sql
@@ -0,0 +1,89 @@
+/*
+SQLyog Ultimate v9.50 
+MySQL - 5.5.16 : Database - ps3
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ps3` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ps3`;
+
+/*Table structure for table `device` */
+
+DROP TABLE IF EXISTS `device`;
+
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `device` */
+
+/*Table structure for table `extra` */
+
+DROP TABLE IF EXISTS `extra`;
+
+CREATE TABLE `extra` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `cost` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `extra` */
+
+/*Table structure for table `payment` */
+
+DROP TABLE IF EXISTS `payment`;
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `device` int(11) DEFAULT NULL,
+  `start` int(11) DEFAULT NULL,
+  `stop` int(11) DEFAULT NULL,
+  `date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `payment` */
+
+/*Table structure for table `payment_extra` */
+
+DROP TABLE IF EXISTS `payment_extra`;
+
+CREATE TABLE `payment_extra` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `extra` int(11) DEFAULT NULL,
+  `number_of` int(11) DEFAULT NULL,
+  `date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `payment_extra` */
+
+/*Table structure for table `system` */
+
+DROP TABLE IF EXISTS `system`;
+
+CREATE TABLE `system` (
+  `var` varchar(255) NOT NULL,
+  `val` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`var`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `system` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
