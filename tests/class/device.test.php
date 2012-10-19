515626058bebf41d19dc4802ec4bebbad0cb6045
tiennguyen <tiennguyen@success-ss.com.vn>
2012-10-19 08:28:42 +0700

    - add unit test

---
+++ tests/class/device.test.php
@@ -0,0 +1,12 @@
+<?php
+
+//include_once dirname(dirname(dirname(__FILE__)));
+class DeviceTest extends PHPUnit_Framework_TestCase
+{
+	public function testCanDo()
+	{
+		die('aaa');
+		//echo dirname(dirname(dirname(__FILE__)));die;
+		$this->assertTrue(true);
+	}
+}
